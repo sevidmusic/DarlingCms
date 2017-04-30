ed6035e0df809255602e00b2b13d5cc94232d403
sevidmusic <sevidmusic@gmail.com>
2017-04-29 20:43:30 -0400

    darlingCms_0.0_dev|core|classes: Modified dev code in index.php.

--- index.php
+++ index.php
@@ -2,47 +2,134 @@
 /**
  * Copyright (c) 2017. Sevi Donnelly Foreman
  */
-$pageName = ucwords(filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING));
-$rootUrl = __DIR__ . '/';
-echo "<!DOCTYPE html>
-<html>
-  <head>
-    <title>$pageName</title>
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/darlingCms/darlingCms.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloWorld/helloWorld.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloUniverse/helloUniverse.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/crudTester/crudTester.css\">
-  </head>
-  <body>
-";
-/** Require Composer's auto-loader. **/
-require(__DIR__ . '/vendor/autoload.php');
 
-$app1 = new \DarlingCms\classes\component\app('varDumper');
-$app1->enableApp();
-$app2 = new \DarlingCms\classes\component\app('helloWorld');
-$app2->enableApp();
-$app3 = new \DarlingCms\classes\component\app('crudTester');
-$app3->enableApp();
-$app4 = new \DarlingCms\classes\component\app('phpCanvas');
-$app4->enableApp();
+/* DEV CODE */
+function displayDevFooter()
+{
 
-$singleAppStartup1 = new \DarlingCms\classes\startup\singleAppStartup($app1);
-$singleAppStartup2 = new \DarlingCms\classes\startup\singleAppStartup($app2);
-$singleAppStartup3 = new \DarlingCms\classes\startup\singleAppStartup($app3);
-$singleAppStartup4 = new \DarlingCms\classes\startup\singleAppStartup($app4);
+    $buttonStyles = "top: 0;position:fixed;display: block;";
 
-$multiAppStartup = new \DarlingCms\classes\startup\multiAppStartup($singleAppStartup1, $singleAppStartup2, $singleAppStartup3, $singleAppStartup4);
-
-$startupObject = new \DarlingCms\classes\startup\darlingCmsStartup($multiAppStartup);
-$startupObject->startup();
-
-if (function_exists('huDebug')) {
-    huDebug($startupObject, 'Startup Object');
-}
-
-echo "
+    $duration = "slow";
+    echo "
+      <button style='$buttonStyles' id='hideLinks'>Show / Hide Links</button>
+      <script>
+        $( \"#hideLinks\" ).click(function() {
+        $( \"a\" ).fadeToggle( \"$duration\" );
+      });
+    </script>
+    
+    <button style='{$buttonStyles}left:10%;' id='hideContent'>Show / Hide Content</button>
+      <script>
+        $( \"#hideContent\" ).click(function() {
+        $( \"p\" ).slideToggle( \"$duration\" );
+        $( \"div\" ).slideToggle( \"$duration\" );
+        $( \"h1\" ).slideToggle( \"$duration\" );
+        $( \"h2\" ).slideToggle( \"$duration\" );
+        $( \"h3\" ).slideToggle( \"$duration\" );
+        $( \"h4\" ).slideToggle( \"$duration\" );
+        $( \"h5\" ).slideToggle( \"$duration\" );
+        $( \"h6\" ).slideToggle( \"$duration\" );
+      });
+    </script>    
+    
+    <button style='{$buttonStyles}left:20%;' id='hideDivs'>Show / Hide Divs</button>
+      <script>
+        $( \"#hideDivs\" ).click(function() {
+        $( \"div\" ).fadeToggle( \"$duration\" );
+      });
+    </script>
+    
+    <button style='{$buttonStyles}left:30%;' id='hideTable'>Show / Hide Tables</button>
+      <script>
+        $( \"#hideTable\" ).click(function() {
+        $( \"table\" ).slideToggle( \"$duration\" );
+      });
+    </script>
+    
+    <button style='{$buttonStyles}left:40%;' id='hideMenus'>Show / Hide Side Menus</button>
+      <script>
+        $( \"#hideMenus\" ).click(function() {
+        $( \"#helloUniverse\" ).slideToggle( \"$duration\" );
+        $( \"#helloWorld\" ).slideToggle( \"$duration\" );
+      });
+    </script>
+    
   </body>
 </html>
 ";
 
+}
+
+function displayDevHead()
+{
+    $pageName = ucwords(filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING));
+
+    echo "<!DOCTYPE html>
+<html>
+  <head>
+    <title>$pageName</title>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/appManager/appManager.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/darlingCms/darlingCms.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloWorld/helloWorld.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloUniverse/helloUniverse.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/crudTester/crudTester.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/varDumper/varDumper.css\">
+    <script src=\"https://code.jquery.com/jquery-1.10.2.js\"></script>
+  </head>
+  <body>
+";
+}
+
+displayDevHead();
+
+/* END DEV CODE */
+/* NOTE: At the moment the code in the index file is just dev code. Until the various
+         core objects are finished, the logic of the index file will remain in dev. */
+
+/** Require Composer's auto-loader. **/
+require(__DIR__ . '/vendor/autoload.php');
+
+/* Ensure appManager is always loaded, for the moment it is responsible for initializing core apps. */
+$appManager = new  \DarlingCms\classes\component\app('appManager');
+$appManager->enableApp();
+$appManager->registerTheme('appManager');
+
+/* Initialize singleAppStartup() object for appManager app. */
+$aps = new \DarlingCms\classes\startup\singleAppStartup($appManager);
+
+/* Initialize multiAppStartup() object. */
+$mas = new  \DarlingCms\classes\startup\multiAppStartup($aps);
+
+/* Initialize crud to read app components from storage. */
+$crud = new \DarlingCms\classes\crud\registeredJsonCrud();
+
+/* Initialize array of the names of the apps to startup. */
+$apps = array('varDumper', 'helloUniverse', 'helloWorld', 'crudTester', 'phpCanvas');
+
+$appStartupObjects = array();
+
+foreach ($apps as $appName) {
+    if ($crud->read($appName) !== false) {
+        $app = $crud->read($appName);
+        array_push($appStartupObjects, new \DarlingCms\classes\startup\singleAppStartup($app));
+    }
+}
+
+foreach ($appStartupObjects as $appStartupObject) {
+    $mas->setStartupObject($appStartupObject);
+}
+
+$mas->startup();
+
+/*
+ *  NOTE : The following code best reflects what the final logic of the index file will look like.
+
+/* Initialize darlingCmsStartup() object * /
+$su = new \DarlingCms\classes\startup\darlingCmsStartup($mas);
+
+/* Startup the Darling Cms.  * /
+$su->startup();
+
+*/
+
+displayDevFooter();
