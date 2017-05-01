7379f67436b0ca7f207d470a35f9ebc0d133caf1
sevidmusic <sevidmusic@gmail.com>
2017-05-01 09:08:08 -0400

    darlingCms_0.0_dev|core|classes: Modified index.php's dev CDN inclusions of jQuery and jQuery ui.

--- index.php
+++ index.php
@@ -74,8 +74,14 @@ function displayDevHead()
     <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloUniverse/helloUniverse.css\">
     <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/crudTester/crudTester.css\">
     <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/varDumper/varDumper.css\">
-    <script src=\"https://code.jquery.com/jquery-1.10.2.js\"></script>
-  </head>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/phpCanvas/phpCanvas.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/jQueryUi/jquery-ui.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/jQueryUi/jquery-ui.structure.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/jQueryUi/jquery-ui.theme.css\">
+    <!-- Load jQuery and jQueryUi from appropriate CDN -->
+    <script src=\"https://code.jquery.com/jquery-3.2.1.js\" integrity=\"sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=\" crossorigin=\"anonymous\"></script>
+    <script src=\"https://code.jquery.com/ui/1.12.1/jquery-ui.js\" integrity=\"sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=\" crossorigin=\"anonymous\"></script>
+</head>
   <body>
 ";
 }
