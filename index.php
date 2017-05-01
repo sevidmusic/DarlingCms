f1f918fade9403ed3119abbfe6930dddcded8583
sevidmusic <sevidmusic@gmail.com>
2017-05-01 11:57:02 -0400

    darlingCms_0.0_dev|core|classes: Modified index.php's dev code.

--- index.php
+++ index.php
@@ -3,92 +3,6 @@
  * Copyright (c) 2017. Sevi Donnelly Foreman
  */
 
-/* DEV CODE */
-function displayDevFooter()
-{
-
-    $buttonStyles = "top: 0;position:fixed;display: block;";
-
-    $duration = "slow";
-    echo "
-      <button style='$buttonStyles' id='hideLinks'>Show / Hide Links</button>
-      <script>
-        $( \"#hideLinks\" ).click(function() {
-        $( \"a\" ).fadeToggle( \"$duration\" );
-      });
-    </script>
-    
-    <button style='{$buttonStyles}left:10%;' id='hideContent'>Show / Hide Content</button>
-      <script>
-        $( \"#hideContent\" ).click(function() {
-        $( \"p\" ).slideToggle( \"$duration\" );
-        $( \"div\" ).slideToggle( \"$duration\" );
-        $( \"h1\" ).slideToggle( \"$duration\" );
-        $( \"h2\" ).slideToggle( \"$duration\" );
-        $( \"h3\" ).slideToggle( \"$duration\" );
-        $( \"h4\" ).slideToggle( \"$duration\" );
-        $( \"h5\" ).slideToggle( \"$duration\" );
-        $( \"h6\" ).slideToggle( \"$duration\" );
-      });
-    </script>    
-    
-    <button style='{$buttonStyles}left:20%;' id='hideDivs'>Show / Hide Divs</button>
-      <script>
-        $( \"#hideDivs\" ).click(function() {
-        $( \"div\" ).fadeToggle( \"$duration\" );
-      });
-    </script>
-    
-    <button style='{$buttonStyles}left:30%;' id='hideTable'>Show / Hide Tables</button>
-      <script>
-        $( \"#hideTable\" ).click(function() {
-        $( \"table\" ).slideToggle( \"$duration\" );
-      });
-    </script>
-    
-    <button style='{$buttonStyles}left:40%;' id='hideMenus'>Show / Hide Side Menus</button>
-      <script>
-        $( \"#hideMenus\" ).click(function() {
-        $( \"#helloUniverse\" ).slideToggle( \"$duration\" );
-        $( \"#helloWorld\" ).slideToggle( \"$duration\" );
-      });
-    </script>
-    
-  </body>
-</html>
-";
-
-}
-
-function displayDevHead()
-{
-    $pageName = ucwords(filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING));
-
-    echo "<!DOCTYPE html>
-<html>
-  <head>
-    <title>$pageName</title>
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/appManager/appManager.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/darlingCms/darlingCms.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloWorld/helloWorld.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/helloUniverse/helloUniverse.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/crudTester/crudTester.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/varDumper/varDumper.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/phpCanvas/phpCanvas.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/jQueryUi/jquery-ui.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/jQueryUi/jquery-ui.structure.css\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"themes/jQueryUi/jquery-ui.theme.css\">
-    <!-- Load jQuery and jQueryUi from appropriate CDN -->
-    <script src=\"https://code.jquery.com/jquery-3.2.1.js\" integrity=\"sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=\" crossorigin=\"anonymous\"></script>
-    <script src=\"https://code.jquery.com/ui/1.12.1/jquery-ui.js\" integrity=\"sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=\" crossorigin=\"anonymous\"></script>
-</head>
-  <body>
-";
-}
-
-displayDevHead();
-
-/* END DEV CODE */
 /* NOTE: At the moment the code in the index file is just dev code. Until the various
          core objects are finished, the logic of the index file will remain in dev. */
 
@@ -110,7 +24,7 @@ $mas = new  \DarlingCms\classes\startup\multiAppStartup($aps);
 $crud = new \DarlingCms\classes\crud\registeredJsonCrud();
 
 /* Initialize array of the names of the apps to startup. */
-$apps = array('varDumper', 'helloUniverse', 'helloWorld', 'crudTester', 'phpCanvas');
+$apps = array('htmlHeadManager', 'varDumper', 'helloUniverse', 'helloWorld', 'crudTester', 'cveReporter', 'phpCanvas', 'htmlFooterManager');
 
 $appStartupObjects = array();
 
@@ -137,5 +51,3 @@ $su = new \DarlingCms\classes\startup\darlingCmsStartup($mas);
 $su->startup();
 
 */
-
-displayDevFooter();
