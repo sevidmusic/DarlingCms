f1533c130d25f3922499d438a3887ae20e96d62e
sevidmusic <sevidmusic@gmail.com>
2017-05-05 10:09:29 -0400

    darlingCms_0.0_dev|core|abstractions: Defined new abstract class Ainitializer under namespace \DarlingCms\abstractions\initializer. The Ainitializer implements the \DarlingCms\interfaces\initializer\Iinitializer interface.

---
+++ core/abstractions/initializer/Ainitializer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sevidmusic
+ * Date: 5/4/17
+ * Time: 5:22 PM
+ */
+
+namespace DarlingCms\abstractions\initializer;
+
+
+abstract class Ainitializer implements \DarlingCms\interfaces\initializer\Iinitializer
+{
+    private $initialized;
+
+    /**
+     * @return bool True if initialization was successful, false otherwise.
+     */
+    abstract public function initialize();
+
+    /**
+     * @return array Array of things that were initialized.
+     */
+    public function getInitialized()
+    {
+        return $this->initialized;
+    }
+
+}
