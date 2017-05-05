9fb0736e5610bf78ea2a7d06454d3c85c237d724
sevidmusic <sevidmusic@gmail.com>
2017-05-05 19:24:07 -0400

    darlingCms_0.0_dev|core|classes: Defined new class dcmsInitializer under namespace DarlingCms\classes\initializer. The dcmsInitializer extends the DarlingCms\abstractions\initializer\Ainitializer abstract class. Refactored index.php to use the new dsmcInitializer in it's logic.

---
+++ core/classes/initializer/dcmsInitializer.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sevidmusic
+ * Date: 5/5/17
+ * Time: 10:15 AM
+ */
+
+namespace DarlingCms\classes\initializer;
+
+
+class dcmsInitializer extends \DarlingCms\abstractions\initializer\Ainitializer
+{
+    /**
+     * @var array
+     */
+    //protected $initialized; // @todo: IMPORTANT!!! change parent's $initialized property's visibility to protected so children do not have to overwrite it as seen here!
+    private $crud;
+
+    /**
+     * initializer constructor.
+     */
+    public function __construct(\DarlingCms\abstractions\crud\AregisteredCrud $crud)
+    {
+        $this->initialized = array();
+        $this->crud = $crud;
+        $this->setInitialized($this->crud, 'crud');
+    }
+
+    /**
+     * Add an item to the initialized array.
+     * @param mixed $item The initialized item.
+     * @param string $index (optional) An optional index to assign to the item.
+     * @return bool True if item was added to the initialized array false otherwise.
+     */
+    private function setInitialized($item, string $index = '')
+    {
+        if ($index !== '') {
+            switch (gettype($item)) {
+                case 'object':
+                    $classification = trim(get_class($item));
+                    $this->initialized[$classification][$index] = $item;
+                    break;
+                default:
+                    $classification = trim(gettype($item));
+                    $this->initialized[$classification][$index] = $item;
+                    break;
+            }
+            return true;
+        }
+
+        switch (gettype($item)) {
+            case 'object':
+                $classification = trim(get_class($item));
+                $this->initialized[$classification][] = $item;
+                break;
+            default:
+                $classification = trim(gettype($item));
+                $this->initialized[$classification][] = $item;
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * @return bool True if initialization was successful, false otherwise.
+     */
+    public function initialize()
+    {
+        /* Initialize appStartupObjects. */
+        $this->initAppStartupObjects();
+
+        return true;
+    }
+
+    /**
+     * Initialize singleAppStartup object instances for any apps
+     * registered with the crud.
+     */
+    private function initAppStartupObjects()
+    {
+        /* Initialize appStartupObjects array. */
+        $appStartupObjects = array();
+
+        /* Search registry for any data classified as an app component. */
+        foreach ($this->crud->getRegistry() as $storageId => $registryData) {
+            /* Check if the data's classification indicates an app component. */
+            if ($this->crud->getRegistryData($storageId, 'classification') === 'DarlingCms\classes\component\app') {
+                /* Load the app component. */
+                if (($app = $this->crud->read($storageId)) !== false) {
+                    /* Create a singleAppStartup instance for the app component and add it to the $appStartupObjects array. */
+                    array_push($appStartupObjects, new \DarlingCms\classes\startup\singleAppStartup($app));
+                }
+            }
+        }
+
+        /* Determine if this is a fresh install of the Darling Cms based on whether
+           or not any single app startup objects were instantiated for any app
+           components registered with the crud. */
+        if ((empty($appStartupObjects)) === true) {
+            return $this->handleFreshInstall();
+        }
+
+        /* Add app startup objects array to the initialized array. */
+        return $this->setInitialized($appStartupObjects, 'appStartupObjects');
+    }
+
+    /**
+     * Performs required actions for a fresh Darling Cms install. Specifically, instantiates
+     * a new app component and singleAppStartup startup object for the "appManager" app.
+     *
+     * Note: The appManager app is responsible for managing installed Darling Cms apps, and therefore,
+     * is assigned the responsibility of enabling and configuring core apps for a fresh install.
+     *
+     * WARNING: The appManager app comes pre-installed with the Darling Cms. If it was removed, configuration
+     * of a new installation will have to be done manually.
+     *
+     */
+    private function handleFreshInstall()
+    {
+        /* Temporarily enable appManager app for fresh install. */
+        $appManager = new  \DarlingCms\classes\component\app('appManager');
+        $appManager->enableApp();
+        $appManager->registerTheme('appManager');
+        /* Initialize singleAppStartup() object for appManager app, wrap in an array and overwrite the initialized array's
+           appStartupObjects array. */
+        return $this->setInitialized(array(new \DarlingCms\classes\startup\singleAppStartup($appManager)), 'appStartupObjects');
+    }
+}
--- index.php
+++ index.php
@@ -6,41 +6,22 @@
 /** Require Composer's auto-loader. **/
 require(__DIR__ . '/vendor/autoload.php');
 
-/* Initialize crud object. */
-$crud = new \DarlingCms\classes\crud\registeredJsonCrud();
+/* Initialize the Darling Cms. */
+$initializer = new \DarlingCms\classes\initializer\dcmsInitializer(new \DarlingCms\classes\crud\registeredJsonCrud());
+$initializer->initialize();
+$initialized = $initializer->getInitialized();
+$appStartupObjects = $initialized['array']['appStartupObjects'];
 
-/* Initialize appStartupObjects array. */
-$appStartupObjects = array();
+if (empty($appStartupObjects) !== true) {
+    /* Initialize multiAppStartup() object, passing the first singleAppStartup object to the constructor. */
+    $mas = new  \DarlingCms\classes\startup\multiAppStartup(array_shift($appStartupObjects));
 
-/* Search registry for any data classified as an app component. */
-foreach ($crud->getRegistry() as $storageId => $registryData) {
-    /* Check if the data's classification indicates an app component. */
-    if ($crud->getRegistryData($storageId, 'classification') === 'DarlingCms\classes\component\app') {
-        /* Load the app component. */
-        if (($app = $crud->read($storageId)) !== false) {
-            /* Create a singleAppStartup instance for the app component and add it to the $appStartupObjects array. */
-            array_push($appStartupObjects, new \DarlingCms\classes\startup\singleAppStartup($app));
-        }
+
+    /* Add the rest of the app startup objects to the multiAppStartup object. */
+    foreach ($appStartupObjects as $appStartupObject) {
+        $mas->setStartupObject($appStartupObject);
     }
+
+    /* Startup the apps. */
+    $mas->startup();
 }
-
-/* Determine if this is a fresh install of the Darling Cms based on whether or not any app components were found. */
-if ((empty($appStartupObjects)) === true) {
-    /* Temporarily enable appManager app for fresh install. */
-    $appManager = new  \DarlingCms\classes\component\app('appManager');
-    $appManager->enableApp();
-    $appManager->registerTheme('appManager');
-    /* Initialize singleAppStartup() object for appManager app, and add it to the $appStartupObjects array. */
-    array_push($appStartupObjects, new \DarlingCms\classes\startup\singleAppStartup($appManager));
-}
-
-/* Initialize multiAppStartup() object, passing the first singleAppStartup object to the constructor. */
-$mas = new  \DarlingCms\classes\startup\multiAppStartup(array_shift($appStartupObjects));
-
-/* Add the rest of the app startup objects to the multiAppStartup object. */
-foreach ($appStartupObjects as $appStartupObject) {
-    $mas->setStartupObject($appStartupObject);
-}
-
-/* Startup the apps. */
-$mas->startup();
