27295b65e7710c5a73bbba2d7bf0ee059cbacacd
sevidmusic <sevidmusic@gmail.com>
2017-05-15 00:50:34 -0400

    darlingCms_0.0_dev|core|classes: Defined new class html under namespace \DarlingCms\classes\component which can be used to generate well formatted html strings. Also, defined new class htmlContainer under namespace \DarlingCms\classes\component which extends the new html class. The htmlContainer class can be used to wrap one or more html components in "containing" tags.

---
+++ core/classes/component/html.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sevidmusic
+ * Date: 5/12/17
+ * Time: 9:10 PM
+ */
+
+namespace DarlingCms\classes\component;
+
+/**
+ * Class html. Defines an object whose responsibility is to generate a string of html based on certain parameters.
+ * This class implements the \DarlingCms\abstractions\component\Acomponent abstract class. This class can be used as
+ * is, or can be used as a base class for more specified html objects. This class does it's best to ensure the html
+ * it generates is valid, standards compliant, html 5.
+ *
+ * @package DarlingCms\classes\component
+ */
+class html extends \DarlingCms\abstractions\component\Acomponent
+{
+    /**
+     * @var string The content that goes between the tags, only used with oc types.
+     */
+    protected $content;
+    //@todo: Review oc and uc arrays to double check tags are in appropriate array and that none are missing...
+    /**
+     * @var string The tag type.
+     */
+    private $tagType;
+    /**
+     * @var array Array of valid html tag types that require an opening and closing html tag.
+     */
+    private $ocTagTypes = array(
+        '!--',
+        'a',
+        'abbr',
+        'address',
+        'article',
+        'aside',
+        'audio',
+        'b',
+        'bdi',
+        'bdo',
+        'blockquote',
+        'body',
+        'button',
+        'canvas',
+        'caption',
+        'cite',
+        'code',
+        'colgroup',
+        'datalist',
+        'dd',
+        'del',
+        'details',
+        'dfn',
+        'dialog',
+        'div',
+        'dl',
+        'dt',
+        'em',
+        'fieldset',
+        'figcaption',
+        'figure',
+        'footer',
+        'form',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'head',
+        'header',
+        'html',
+        'i',
+        'iframe',
+        'ins',
+        'kbd',
+        'label',
+        'legend',
+        'li',
+        'menu', /* Firefox >=8 only... */
+        'menuitem', /* Firefox >= 8 only... */
+        'meter',
+        'nav',
+        'noscript',
+        'object',
+        'ol',
+        'optgroup',
+        'option',
+        'output',
+        'p',
+        'picture',
+        'pre',
+        'progress',
+        'q',
+        'rp',
+        'rt',
+        'ruby',
+        's',
+        'samp',
+        'script',
+        'section',
+        'select',
+        'small',
+        'sup',
+        'table',
+        'summary',
+        'tbody',
+        'td',
+        'textarea',
+        'tfoot',
+        'th',
+        'thead',
+        'time',
+        'title',
+        'tr',
+        'track',
+        'u',
+        'ul',
+        'var',
+        'video',
+        'p',
+    );
+    /**
+     * @var array Array of valid html tag types that  only require an opening html tag.
+     */
+    private $ucTagTypes = array(
+        'area',
+        'base',
+        'br',
+        'col',
+        'embed',
+        'hr',
+        'img',
+        'input',
+        'keygen',
+        'link',
+        'main',
+        'map',
+        'mark',
+        'meta',
+        'param',
+        'source',
+        'span',
+        'strong',
+        'style',
+        'sub',
+    );
+    /**
+     * @var string The html.
+     */
+    private $html;
+
+    /**
+     * html constructor.
+     * @param string $tagType
+     * @param string $content
+     * @param array $attributes
+     */
+    public function __construct(string $tagType, string $content = '', array $attributes = array())
+    {
+        $this->setComponentAttributes($attributes);
+        $this->setComponentName($tagType);
+        if (in_array($tagType, $this->ocTagTypes, true) || in_array($tagType, $this->ucTagTypes, true)) {
+            $this->tagType = $tagType;
+            $this->content = trim($content);
+            $this->generateHtml();
+        }
+        parent::__construct();
+    }
+
+    /**
+     * Set component attributes array.
+     * @param array $attributes The array to set as the component attributes.
+     * @return bool True if $attributes were set, false otherwise.
+     */
+    public function setComponentAttributes(array $attributes)
+    {
+        $this->componentAttributes = $attributes;
+        return isset($this->componentAttributes);
+    }
+
+    /**
+     * Set the component name.
+     * @param string $name
+     * @return bool True if component name was set, false otherwise.
+     */
+    public function setComponentName(string $name)
+    {
+        if (isset($this->componentName) === false) {
+            $this->componentName = $name;
+        }
+        return (isset($this->componentName) && ($this->componentName === $name));
+    }
+
+    /**
+     * Generates the html.
+     * @return bool True if html was generated and set and assigned to the html property, false otherwise.
+     */
+    public function generateHtml()
+    {
+        return (in_array($this->tagType, $this->ucTagTypes, true) ? $this->generateUcTag() : $this->generateOcTag());
+    }
+
+    /**
+     * Generate html without the closing tag.
+     * @return bool True if html was generated and set and assigned to the html property, false otherwise.
+     */
+    private function generateUcTag()
+    {
+        $attributes = $this->getComponentAttributes();
+        switch (empty($attributes)) {
+            case true:
+                $this->html = PHP_EOL . str_replace(' >', '>', "<$this->tagType>") . PHP_EOL;
+                break;
+            case false:
+                $this->html = PHP_EOL . str_replace(' >', '>', "<$this->tagType " . implode(' ', $this->getComponentAttributes()) . ">") . PHP_EOL;
+                break;
+        }
+        return isset($this->html);
+    }
+
+    /**
+     * Generates html with open and closing tag.
+     * @return bool True if html was generated and set and assigned to the html property, false otherwise.
+     */
+    private function generateOcTag()
+    {
+        $attributes = $this->getComponentAttributes();
+        switch (empty($attributes)) {
+            case true:
+                $this->html = PHP_EOL . str_replace(' >', '>', "<$this->tagType>$this->content</$this->tagType>") . PHP_EOL;
+                break;
+            case false:
+                $this->html = PHP_EOL . str_replace(' >', '>', "<$this->tagType " . implode(' ', $this->getComponentAttributes()) . ">$this->content</$this->tagType>") . PHP_EOL;
+                break;
+        }
+        return isset($this->html);
+    }
+
+    /**
+     * Returns the html via getHtml().
+     *
+     * @return string The html.
+     *
+     */
+    function __toString()
+    {
+        return $this->getHtml();
+    }
+
+    /**
+     * Returns the generated html.
+     * @return string The generated html.
+     */
+    public function getHtml()
+    {
+        return $this->html;
+    }
+
+
+}
---
+++ core/classes/component/htmlContainer.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sevidmusic
+ * Date: 5/13/17
+ * Time: 10:30 AM
+ */
+
+namespace DarlingCms\classes\component;
+
+/**
+ * Class htmlContainer. Responsible for generating "containing" tags for one or more html components.
+ * For example, the following php:
+ * <?php
+ * $c = new htmlContainer('div', array('class="container"'),
+ *                      new html('p', 'hello'),
+ *                      new html('a', 'world', 'href="#world"')
+ * );
+ * echo $c->getHtml();
+ * ?>
+ *
+ * would generate the following html:
+ * <div class="container">
+ * <p>Hello</p>
+ * <a href="#world">world</a>
+ * </div>
+ * @package DarlingCms\classes\component
+ */
+class htmlContainer extends \DarlingCms\classes\component\html
+{
+    /**
+     * htmlContainer constructor.
+     * @param string $tagType The tag type to use for the container.
+     * @param array $attributes Array of attributes for the container.
+     * @param html[] ...$html The html component(s) to be contained.
+     */
+    public function __construct(string $tagType, array $attributes, \DarlingCms\classes\component\html ...$html)
+    {
+        if (is_array($attributes) === false) {
+            $attributes = array();
+        }
+
+        foreach ($html as $htmlComponent) {
+            $this->appendHtml($htmlComponent);
+        }
+
+        parent::__construct($tagType, $this->content, $attributes);
+    }
+
+    /**
+     * Appends an html component's html to the containers content.
+     *
+     * @param html[] ...$htmlComponent The html component(s) to append to the container's content.
+     * @return bool True if successful, false otherwise.
+     */
+    public function appendHtml(\DarlingCms\classes\component\html ...$htmlComponent)
+    {
+        foreach ($htmlComponent as $component) {
+            $this->content .= PHP_EOL . $component->getHtml() . PHP_EOL;
+        }
+        return $this->generateHtml();
+    }
+
+    /**
+     * Prepends an html component's html to the containers content.
+     *
+     * @param html[] ...$htmlComponent The html component(s) to prepend to the containers content.
+     * @return bool True if successful, false otherwise.
+     */
+    public function prependHtml(\DarlingCms\classes\component\html ...$htmlComponent)
+    {
+        array_reverse($htmlComponent);
+        foreach ($htmlComponent as $component) {
+            $this->content = PHP_EOL . $component->getHtml() . PHP_EOL . $this->content . PHP_EOL;
+        }
+        return $this->generateHtml();
+    }
+}
