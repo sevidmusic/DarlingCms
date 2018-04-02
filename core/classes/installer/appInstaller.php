3478409d509c5be834b817511698b0c4f4076eac
sevidmusic <sevidmusic@gmail.com>
2018-04-01 22:05:39 -0500

    darlingCms_0.0_dev|core|classes|installer: Finished implementing the appInstaller class. - Defined new property $htmlHead which holds the htmlHead object instance used by the installer. - The appInstaller::__construct() method now instantiates an htmlHead() object and assigns it to the new $htmlHead property. - Refactored the appInstaller::install() method to enable any themes that are registered with the app being installed via the htmlHead::enableTheme() method. - Defined new method appInstaller::registerDependency() which can be used to register a dependency for the app being installed. - Defined new method appInstaller::registerTheme() which can be used to register a theme for the app being installed. - Defined new method appInstaller::setCustomAttribute() which can be used to set a custom attribute for the app being installed. - Defined new method appInstaller::hasAccessController() which can be used to to determine whether or not the app being installed has an accessController object assigned to it. - Defined new method appInstaller::getAccessController() which can be used to get the accessController assigned to the app being installed if there is in fact an accessController assigned to the app. - Defined new method appInstaller::setAccessController which can be used to set the accessController assigned to the app being installed. - Defined new method appInstaller::getThemes() which can be used to get an array of the themes registered with the app being installed. - Defined new method appInstaller::getDependencies() which can be used to get an array of the dependencies registered with the app being installed. - Defined new method appInstaller::getCustomAttributes() which can be used to get an array of the custom attributes assigned to the app being installed.

--- core/classes/installer/appInstaller.php
+++ core/classes/installer/appInstaller.php
@@ -10,7 +10,9 @@ namespace DarlingCms\classes\installer;
 
 
 use DarlingCms\abstractions\crud\AregisteredCrud;
+use DarlingCms\classes\accessControl\accessController;
 use DarlingCms\classes\component\app;
+use DarlingCms\classes\component\html\htmlHead;
 use DarlingCms\interfaces\installer\Iinstaller;
 
 /**
@@ -39,6 +41,11 @@ class appInstaller implements Iinstaller
     private $appComponent;
 
     /**
+     * @var htmlHead The htmlHead object used by this installer.
+     */
+    private $htmlHead;
+
+    /**
      * appInstaller constructor. Assigns the specified $appName to the $appName property. Assigns the specified
      * AregisteredCrud implementation to the $curd property. Instantiates an app component for the app managed
      * by this installer.
@@ -57,6 +64,8 @@ class appInstaller implements Iinstaller
         $this->crud = $crud;
         /* Instantiate an app component for the app this installer manages, and assign it to the appComponent property. */
         $this->appComponent = new app($appName, $customAttributes);
+        /* Instantiate an htmlHead for this installer. */
+        $this->htmlHead = new htmlHead($this->crud);
     }
 
     /**
@@ -70,6 +79,10 @@ class appInstaller implements Iinstaller
         $status = array();
         /* Enable the app. */
         array_push($status, $this->appComponent->enableApp());
+        /* Enable any themes registered with the app. */
+        foreach ($this->getThemes() as $theme) {
+            array_push($status, $this->htmlHead->enableTheme($theme));
+        }
         /* Store the app component. */
         array_push($status, $this->crud->create($this->appName, $this->appComponent));
         /* Return true on success, false on failure. */
@@ -89,6 +102,99 @@ class appInstaller implements Iinstaller
         array_push($status, $this->crud->create($this->appName, $this->appComponent));
         /* Return true on success, false on failure. */
         return !in_array(false, $status, true);
+    }
+
+    /**
+     * Register a dependency for the app managed by this installer.
+     * @param string $dependency The name of the app to register as a dependency.
+     * Note: An app cannot register itself as a dependency, attempting to register
+     * the app managed by this installer as a dependency will cause this method
+     * to return false.
+     * @return bool True if dependency was registered, false otherwise.
+     */
+    public function registerDependency(string $dependency): bool
+    {
+        return $this->appComponent->registerDependency($dependency);
+    }
+
+    /**
+     * Register a theme for the app managed by this installer.
+     * @param string $theme The name of the theme to register.
+     * @return bool True if theme was registered, false otherwise.
+     */
+    public function registerTheme(string $theme): bool
+    {
+        return $this->appComponent->registerTheme($theme);
+    }
+
+    /**
+     * Set a custom attribute for the app managed by this installer.
+     * @param string $customAttributeKey The key to assign to the custom attribute.
+     * @param mixed $customAttributeValue The value of the custom attribute.
+     * @return bool True if custom attribute was set, false otherwise.
+     */
+    public function setCustomAttribute(string $customAttributeKey, $customAttributeValue): bool
+    {
+        return $this->appComponent->setCustomAttribute($customAttributeKey, $customAttributeValue);
+    }
+
+    /**
+     * Determines if the app managed by this installer is assigned an accessController.
+     * @return bool True if the app managed by this installer is assigned an accesssController, false otherwise.
+     */
+    public function hasAccessController(): bool
+    {
+        return $this->appComponent->hasAccessController();
+    }
+
+    /**
+     * Returns the accessController assigned to the app managed by this installer, or false
+     * if an accessController is not assigned to the app managed by this installer.
+     * @return bool|\DarlingCms\classes\accessControl\accessController The accessController assigned to the
+     *                                                                 app managed by this installer, or false
+     *                                                                 if an accessController is not assigned to
+     *                                                                 the app managed by this installer.
+     */
+    public function getAccessController() /* mixed : accessController or false in none assigned */
+    {
+        return $this->appComponent->getAccessController();
+    }
+
+    /**
+     * Assign an accessController to the app managed by this installer.
+     * @param accessController $accessController The accessController to assign to the app managed by this installer.
+     * @return bool True if the accessController was assigned to the app managed by this installer, false otherwise.
+     */
+    public function setAccessController(accessController $accessController): bool
+    {
+        return $this->appComponent->setAccessController($accessController);
+    }
+
+    /**
+     * Get an array of the themes assigned to the app managed by this installer.
+     * @return array Array of themes assigned to the app managed by this installer.
+     */
+    public function getThemes(): array
+    {
+        return $this->appComponent->getComponentAttributeValue('themes');
+    }
+
+    /**
+     * Get an array of the dependencies assigned to the app managed by this installer.
+     * @return array Array of dependencies assigned to the app managed by this installer.
+     */
+    public function getDependencies(): array
+    {
+        return $this->appComponent->getComponentAttributeValue('dependencies');
+    }
+
+    /**
+     * Get an array of the custom attributes assigned to the app managed by this installer.
+     * @return array Array of the custom attributes assigned to the app managed by this installer.
+     */
+    public function getCustomAttributes(): array
+    {
+        return $this->appComponent->getComponentAttributeValue('customAttributes');
     }
 
 }
