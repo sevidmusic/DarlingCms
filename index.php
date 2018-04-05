ed92b08fd21ae92519a4d36bc913026a6f0f1d19
sevidmusic <sevidmusic@gmail.com>
2018-04-05 18:49:23 -0500

    darlingCms_0.0_dev|index: Refactored index.php to use the \DarlingCms\classes\userInterface\userInterface class to generate it's html.

--- index.php
+++ index.php
@@ -18,24 +18,28 @@ $initialized = $initializer->getInitialized();
 if (isset($initialized['DarlingCms\classes\startup\multiAppStartup']['multiAppStartup']) && gettype($initialized['DarlingCms\classes\startup\multiAppStartup']['multiAppStartup']) === 'object' && get_class($initialized['DarlingCms\classes\startup\multiAppStartup']['multiAppStartup']) === 'DarlingCms\classes\startup\multiAppStartup') {
     /*** Instantiate the htmlHead object used to generate the pages html header. ***/
     /**
-     * NOTE: It is best to instantiate the htmlHead object before app startup to insure changes made to htmlhead
+     * WARNING: It is best to instantiate the htmlHead object before app startup to insure changes made to htmlhead
      * object by apps take precedence over index.php. If instantiated after startup then changes to htmlHead made
      * by index.php will take precedence over changes made by apps. Furthermore, changes made by index.php in
      * general should happen before startup or they will take precedence over apps.
-     * e.g. calling resetHtmlHead() before startup will allow changes made by apps to apply, if called after
-     * startup then any changes made by apps will not apply.
      */
     $htmlHead = new \DarlingCms\classes\component\html\htmlHead($initializer->getCrud());
+    /* Create a meta tag to set the viewport and prepend it to the htmlHead's html container. */
     $htmlHead->prependHtml(new \DarlingCms\classes\component\html\html('meta', '', array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0')));
+    /* Create the user interface. */
+    $userInterface = new \DarlingCms\classes\userInterface\userInterface();
+    /* Set the user interface's container type to "html". */
+    $userInterface->containerType = 'html';
+    /* Add the htmlHead's html container to the user interface's opening html. */
+    $userInterface->addOpeningHtml($htmlHead);
+    /* Add html comment to the userInterface's closing html with the message:
+       Site created with the Darling Cms | View on GitHub @ http://www.github.com/sevidmusic/DarlingCms */
+    $userInterface->addClosingHtml(new \DarlingCms\classes\component\html\html('!--', 'Site created with the Darling Cms | View on GitHub @ http://www.github.com/sevidmusic/DarlingCms'));
     /* Get the multiAppStartup object. */
     $multiAppStartupObject = $initialized['DarlingCms\classes\startup\multiAppStartup']['multiAppStartup'];
     /* Use the multiAppStartup object to startup the apps | * SEE WARNING ABOVE ABOUT OUTPUTTING FROM index.php. */
     $multiAppStartupObject->startup();
-    /*** Display the page. | * SEE WARNING ABOVE ABOUT OUTPUTTING FROM index.php. ***/
-    /* Output opening html | doctype, opening html tag, etc. */
-    echo '<!DOCTYPE html>' . PHP_EOL . '<html>' . $htmlHead . '<body>' . PHP_EOL;
-    /* Display app output from any apps that were started up. */
-    echo $multiAppStartupObject->getAppOutput();
-    /* Output closing html | closing body tag, closing html tag, etc.*/
-    echo PHP_EOL . '</body>' . PHP_EOL . '</html>';
+    /* Create the body html tag, and assign the app output as it's content. */
+    $body = new \DarlingCms\classes\component\html\html('body', $multiAppStartupObject->getAppOutput());
+    echo '<!DOCTYPE html>' . $userInterface->getUserInterface($body);
 }
