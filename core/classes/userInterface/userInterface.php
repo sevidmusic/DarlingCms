f11cbe8ebf0f3ffc93fd315aae485b93da075f58
sevidmusic <sevidmusic@gmail.com>
2018-04-09 08:27:46 -0500

    darlingCms_0.0_dev|dcmsCore: Added or revised doc comments for the \DarlingCms\classes\userInterface\userInterface class.

--- core/classes/userInterface/userInterface.php
+++ core/classes/userInterface/userInterface.php
@@ -14,9 +14,20 @@ use DarlingCms\interfaces\userInterface\IuserInterface;
  */
 class userInterface implements IuserInterface
 {
-    /* Position Constants */
+    /**
+     * @var int Used to set position of html objects that should be incorporated into the opening html.
+     */
     const OPENING = 1;
+
+    /**
+     * @var int Used to set position of html objects that should be incorporated into the html in between
+     *          the opening html and the closing html.
+     */
     const MIDDLE = 2;
+
+    /**
+     * @var int Used to set position of html objects that should be incorporated into the closing html.
+     */
     const CLOSING = 4;
 
     /**
@@ -46,7 +57,7 @@ class userInterface implements IuserInterface
     public $containerType = 'div';
 
     /**
-     * userInterface constructor. Set the user interface's html container's id attribute. Assigns each
+     * userInterface constructor. Sets the user interface's html container's id attribute. Assigns each
      * of the specified css class names to the user interface's html container's class attribute.
      * @param string $userInterfaceHtmlId (optional) The html id to assign to the user interface's html container.
      * @param array $userInterfaceCssClasses (optional) Array of css class names to assign to the user interface's
@@ -67,13 +78,17 @@ class userInterface implements IuserInterface
 
     /**
      * Returns the html container for this user interface.
-     * Note: If this method is called before the addOpeningHtml() or addClosingHtml() methods
-     * any html objects added by those methods will not be included in the user interface's
-     * htmlContainer.
+     * Note: If this method is called before the addOpeningHtml(), addClosingHtml(), addPageContent(),
+     * addContent(), or addHtml() methods, any html objects added by those methods will not be included
+     * in the user interface's htmlContainer. i.e., the add*() methods must be called before getUserInterface()
+     * to have their additions included in the user interface.
      * @param html ...$html The html objects to append the user interface's htmlContainer.
-     * Note: Any html objects passed to getUserInterface() will be added in between the
-     * html objects that were added via the addOpeningHtml() and addClosingHtml()
-     * methods respectively.
+     * Note: Any html objects passed directly to getUserInterface() will be incorporated in between the
+     * html objects that were added via the addOpeningHtml() and addClosingHtml() methods respectively.
+     * i.e., Objects passed directly to getUserInterface() will assume the MIDDLE position.
+     * Note: Any objects passed directly to getUserInterface() will be incorporated after html objects
+     * that were added via the addPageContent(), addContent(), or addHtml() method and were assigned to
+     * the MIDDLE position.
      * @return htmlContainer Returns the htmlContainer instance responsible for this user interface's html.
      */
     public function getUserInterface(html ...$html): htmlContainer
@@ -170,18 +185,20 @@ class userInterface implements IuserInterface
     }
 
     /**
-     * Add content to specified pages.
+     * Add html content to specified pages.
      * @param array $pages Array of page names to add the content to.
      * @param html $content The html object instance responsible for the content's html.
-     * @param string $position (optional) If set, determines if the content will be added to the user interface's
-     *                                    opening html, middle html, or closing html. If not set the content will be added
-     *                                    to user interface between the opening and closing html.
-     *                                    Options:
-     *                                      1 = Add to opening html. (Hint: use class constant userInterface::OPENING)
-     *                                      2 = Add to closing html. (Hint: userInterface::CLOSING)
-     *
+     * @param int $position (optional) If set, these flags are used to determine the position(s)
+     *                                 the html content will be assigned to.
+     *                                 Note: Multiple position flags can be used to assign the html
+     *                                 content to multiple positions.
+     *                                 -- Options --
+     *                                 OPENING: If set, html content will be assigned to the opening html.
+     *                                 MIDDLE: If set, html content will be assigned to the the middle html,
+     *                                         i.e., between the OPENING and CLOSING html.
+     *                                 CLOSING: If set, html content will be assigned to the closing html.
      * @return bool True if content was added, false otherwise. Note: This method will return false
-     *              when the current page is not one of the pages specified by the $pages parameter.
+     *              if the current page is not one of the pages specified by the $pages parameter.
      */
     public function addPageContent(array $pages, html $content, $position = 2): bool
     {
@@ -192,6 +209,21 @@ class userInterface implements IuserInterface
         return false;
     }
 
+
+    /**
+     * Add html content.
+     * @param html $content The html content to add.
+     * @param int $position (optional) If set, these flags are used to determine the position(s)
+     *                                 the html content will be assigned to.
+     *                                 Note: Multiple position flags can be used to assign the html
+     *                                 content to multiple positions.
+     *                                 -- Options --
+     *                                 OPENING: If set, html content will be assigned to the opening html.
+     *                                 MIDDLE: If set, html content will be assigned to the the middle html,
+     *                                         i.e., between the OPENING and CLOSING html.
+     *                                 CLOSING: If set, html content will be assigned to the closing html.
+     * @return bool True if the content was added, false otherwise.
+     */
     public function addContent(html $content, $position = 2): bool
     {
         $status = array();
@@ -210,7 +242,8 @@ class userInterface implements IuserInterface
     /**
      * Add html objects to the user interface's html container.
      * Note: Html objects added with this method will be appended to the user interface's htmlContainer
-     *       between the html objects assigned to the $openingHtml and $closingHtml property arrays.
+     * between the html objects assigned to the $openingHtml and $closingHtml property arrays, i.e.,
+     * the MIDDLE position.
      * Note: This method must be called before the getUserInterface() method or it will have no effect.
      * @param html $html The html object to added to the user interface's html container.
      * @return bool True if html objects were added, false otherwise.
