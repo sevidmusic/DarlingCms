b4ca6c8488194c38c3e0066c35983b8a84a1d19a
sevidmusic <sevidmusic@gmail.com>
2018-04-17 12:00:01 -0500

    darlingCms_0.0_dev|dcmsCore: Refactored the \DarlingCms\classes\initializer\dcmsInitializer class, - Added and revised doc comments. - Added return type declarations where appropriate. - Refactored the dcmsInitializer::initialize() method to use an array to track the success of each of the method's it calls. Now, as long as all the methods it calls return true, it will return true, if not, it will return false. - Refactored the dcmsInitializer::initMultiAppStartupObject() method: --> Now, if there aren't any singleAppStart objects initialized, the initMultiAppStartupObject() method will log an error indicating the Darling Cms failed to start, and return false. --> Now, if the multiAppStartup object could not be initialized, the initMultiAppStartupObject() method will log an error indicating the Darling Cms failed to start, and return false. --> Implemented try catch for the ReflectionClass used by the initMultiAppStartupObject() method. --> Now, the initMultiAppStartupObject() method returns true if it's call to the dcmsInitializer::setInitialized() method returned true, false otherwise. - Refactored the dcmsInitializer::initAppStartupObjects() method to use the AregisteredCrud::read() method's $classification parameter to check if data is an app object. - Refactored the dcmsInitializer::handleFreshInstall() method to use an instance of an appInstaller() object to install apps that do not provide an installer.php file.

--- core/classes/initializer/dcmsInitializer.php
+++ core/classes/initializer/dcmsInitializer.php
@@ -11,51 +11,62 @@ namespace DarlingCms\classes\initializer;
 use DarlingCms\abstractions\crud\AregisteredCrud;
 use DarlingCms\abstractions\initializer\Ainitializer;
 use DarlingCms\classes\component\html\html;
+use DarlingCms\classes\installer\appInstaller;
 
 /**
  * Class dcmsInitializer. Responsible for initializing any data needed by the Darling Cms. Initialized data
  * is stored in an array which can be retrieved by calling the getInitialized() method defined by the parent
  * Ainitializer abstract class.
  * @package DarlingCms\classes\initializer
- * @see Ainitializer::getInitialized()
+ * @see \DarlingCms\abstractions\initializer\Ainitializer::getInitialized()
  */
 class dcmsInitializer extends Ainitializer
 {
     /**
-     * @var \DarlingCms\abstractions\crud\AregisteredCrud Registered Crud object used by the dcmsInitializer.
+     * @var \DarlingCms\abstractions\crud\AregisteredCrud Instance of an AregisteredCrud implementation
+     *                                                    used by the dcmsInitializer to interact with
+     *                                                    storage.
      */
     private $crud;
 
     /**
-     * dcmsInitializer constructor. Assigns the provided registered crud implementation to the $crud property,
-     * and also sets it as one of the initialized items under the index "crud".
+     * dcmsInitializer constructor. Assigns the specified AregisteredCrud implementation instance to
+     * the $crud property.
      *
-     * @param \DarlingCms\abstractions\crud\AregisteredCrud $crud Instance of an object that implements the \DarlingCms\abstractions\crud\AregisteredCrud abstract class.
+     * @param \DarlingCms\abstractions\crud\AregisteredCrud $crud Instance of an object that implements the
+     *                                                            \DarlingCms\abstractions\crud\AregisteredCrud
+     *                                                            abstract class.
+     * @see \DarlingCms\abstractions\crud\AregisteredCrud
      */
     public function __construct(AregisteredCrud $crud)
     {
-        /* Assign the provided registered crud implementation to the $crud property. */
+        /* Assign the provided AregisteredCrud implementation instance to the $crud property. */
         $this->crud = $crud;
     }
 
     /**
-     * Returns the AregisteredCrud implementation assigned to the initializer.
+     * Returns the AregisteredCrud implementation instance assigned to the dcmsInitializer's $crud property.
      * @return \DarlingCms\abstractions\crud\AregisteredCrud The AregisteredCrud implementation assigned
-     *                                                       to the initializer.
+     *                                                       to the dcmsInitializer's $crud property.
      * @see AregisteredCrud
      */
-    public function getCrud()
+    public function getCrud(): AregisteredCrud
     {
         return $this->crud;
     }
 
     /**
      * Add an item to the initialized array.
-     * @param mixed $item The initialized item.
+     * @param mixed $item The item.
      * @param string $index (optional) An optional index to assign to the item.
-     * @return bool True if item was added to the initialized array false otherwise.
+     * Note: Data in the $initialized property's array is always indexed first by classification, then by the
+     * appropriate index. Data is classified based on type, or fully qualified class name for objects.
+     * i.e., $initialize[type][INDEX] or $initialized[\Namespace\ClassName][INDEX]
+     * @return bool True if item was added to the initialized array, false otherwise.
+     * @see dcmsInitializer::handleAssociativeIndex()
+     * @see dcmsInitializer::handleNumericIndex()
      */
-    private function setInitialized($item, string $index = '')
+    private function setInitialized($item, string $index = ''): bool
     {
         if ($index !== '') {
             return $this->handleAssociativeIndex($index, $item);
@@ -65,11 +76,14 @@ class dcmsInitializer extends Ainitializer
 
     /**
      * Handle associative indexing of an item in the initialized array.
-     * @param string $index The index to use.
+     * Note: Data added to the $initialized property's array by this method is always indexed first by classification,
+     * then by the specified $index. Data is classified based on type, or fully qualified class name for objects.
+     * i.e., $initialize[type][$index] or $initialized[\Namespace\ClassName][$index]
+     * @param string $index The index to assign to the data.
      * @param mixed $item The item being set in the initialized array.
      * @return bool True if item was set in the initialized array under the specified index, false otherwise.
      */
-    private function handleAssociativeIndex(string $index, $item)
+    private function handleAssociativeIndex(string $index, $item): bool
     {
         /* Handle associative index. */
         switch (gettype($item)) {
@@ -86,11 +100,14 @@ class dcmsInitializer extends Ainitializer
 
     /**
      * Handle numeric indexing of an item in the initialized array.
+     * Note: Data added to the $initialized property's array by this method is always indexed first by classification,
+     * then by the numeric index. Data is classified based on type, or fully qualified class name for objects.
+     * i.e., $initialize[type][NUMERIC_INDEX] or $initialized[\Namespace\ClassName][NUMERIC_INDEX]
      * Note: This numeric index will be generated internally.
      * @param mixed $item The item being set in the initialized array.
      * @return bool True if item was set in the initialized array, false otherwise.
      */
-    private function handleNumericIndex($item)
+    private function handleNumericIndex($item): bool
     {
         /* Handle numeric index. */
         switch (gettype($item)) {
@@ -106,67 +123,96 @@ class dcmsInitializer extends Ainitializer
     }
 
     /**
-     * @return bool True if any data other then the crud was initialized, false otherwise.
+     * Initializes the appropriate data. Currently this method initializes the
+     * singleAppStartup objects, and the multiAppStartup object.
+     * @return bool True if data was initialized, false otherwise.
+     * @see dcmsInitializer::initAppStartupObjects()
+     * @see dcmsInitializer::initMultiAppStartupObject()
+     * @see \DarlingCms\classes\startup\singleAppStartup
+     * @see \DarlingCms\classes\startup\multiAppStartup
      */
-    public function initialize()
+    public function initialize(): bool
     {
+        /* Initialize an array to track the success of each method this method calls. */
+        $status = array();
         /* Initialize appStartupObjects. */
-        $this->initAppStartupObjects();
+        array_push($status, $this->initAppStartupObjects());
         /* Initialize multiAppStartup object. */
-        $this->initMultiAppStartupObject();
-        /* Return true if there was any data initialized other then the crud, false otherwise. */
-        return (count($this->initialized) > 1);
+        array_push($status, $this->initMultiAppStartupObject());
+        /* Return true if there was any data initialized, false otherwise. */
+        return in_array(false, $status, true);
     }
 
     /**
      * Initialize the multiAppStartup object.
-     * @return bool True if multiAppStartup object was initialized, false otherwise.
+     *
+     * Note: This method uses PHP's ReflectionClass to instantiate a multiAppStartup object instance.
+     * .
+     * Note: If there are no singleAppStart objects initialized, this method will log an error indicating
+     * the Darling Cms failed to start, and return false.
+     *
+     * Note: If the multiAppStartup object could not be initialized, this method will log an error indicating
+     * the Darling Cms failed to start, and return false.
+     *
+     * @return bool True if multiAppStartup object was instantiated and assigned to the initialized property's array,
+     *              false otherwise.
+     * @see \ReflectionClass
+     * @see \ReflectionException
+     * @see \ReflectionClass::newInstanceArgs()
+     * @see dcmsInitializer::setInitialized()
      */
-    private function initMultiAppStartupObject()
+    private function initMultiAppStartupObject(): bool
     {
-        /* If the array of initialized appStartupObjects was initialized, proceed. */
-        if (isset($this->initialized['array']['appStartupObjects']) === true) {
-            /* Get array of initialized appStartupObjects. */
-            $appStartupObjects = $this->initialized['array']['appStartupObjects'];
-            /* If the array of initialized appStartupObjects is not empty, proceed. */
-            if (empty($appStartupObjects) === false) {
-                /* Instantiate ReflectionClass object for the multiAppStartup instance. */
-                $reflector = new \ReflectionClass('DarlingCms\classes\startup\multiAppStartup');
-                /* Call the ReflectionClass's newInstanceArgs() method in order to pass the initialized
-                 * singleAppStartup objects to the multiAppStartup class's constructor. This will create
-                 * a new instance of a multiAppStartup object. This logic removes the need to loop through
-                 * the array of singleAppStartupObjects.
-                 */
-                $multiAppStartupObject = $reflector->newInstanceArgs($appStartupObjects);
-                /* Add multiAppStartup object to the $initialized property's array. */
-                $this->setInitialized($multiAppStartupObject, 'multiAppStartup');
-            }
+        /* If the array of initialized singleAppStartup objects is not set, or the array of initialized
+           singleAppStartup objects is empty, log an error and return false. */
+        if (isset($this->initialized['array']['appStartupObjects']) === false || empty($this->initialized['array']['appStartupObjects']) === true) {
+            error_log('Darling Cms Startup Error: There are no apps installed. Error occurred in ' . __FILE__ . ' on line ' . (__LINE__ - 1));
+            return false;
         }
-        /* Return true if multiAppStartup object was initialized, false otherwise. */
-        return isset($this->initialized['DarlingCms\classes\startup\multiAppStartup']['multiAppStartup']);
+        /* Instantiate ReflectionClass object for the multiAppStartup instance. */
+        try {
+            $reflector = new \ReflectionClass('DarlingCms\classes\startup\multiAppStartup');
+        } catch (\ReflectionException $e) {
+            /* Failed to instantiate a ReflectionClass object for the multiAppStartup class, log error and
+               return false. */
+            error_log('Darling Cms Startup Error: The Darling Cms failed to start!' . PHP_EOL . $e->getMessage() . ' on line ' . $e->getLine() . ' in ' . $e->getFile() . PHP_EOL . 'Stack Trace:' . PHP_EOL . $e->getTraceAsString() . PHP_EOL . 'Exception Code: ' . $e->getCode() . PHP_EOL);
+            return false;
+        }
+        /* Call the ReflectionClass's newInstanceArgs() method in order to pass the initialized
+         * singleAppStartup objects to the multiAppStartup class's constructor. This will create
+         * a new instance of a multiAppStartup object. This logic removes the need to loop through
+         * the array of singleAppStartupObjects.
+         */
+        $multiAppStartupObject = $reflector->newInstanceArgs($this->initialized['array']['appStartupObjects']);
+        /* Add multiAppStartup object to the $initialized property's array. */
+        return $this->setInitialized($multiAppStartupObject, 'multiAppStartup');
     }
 
     /**
      * Initialize singleAppStartup object instances for any apps
      * registered with the crud.
+     * @return bool True if array of singleAppStartup objects was added to the $initialized property's array,
+     *              false otherwise. If this is a fresh install of the Darling Cms, this method will return
+     *              true if the dcmsInitializer::freshInstall() method returned true, false otherwise.
+     * @see \DarlingCms\abstractions\crud\AregisteredCrud
+     * @see \DarlingCms\abstractions\crud\AregisteredCrud::getRegistry()
+     * @see \DarlingCms\abstractions\crud\AregisteredCrud::read()
+     * @see \DarlingCms\classes\startup\singleAppStartup
+     * @see dcmsInitializer::handleFreshInstall()
+     * @see dcmsInitializer::setInitialized()
      */
-    private function initAppStartupObjects()
+    private function initAppStartupObjects(): bool
     {
-        /* Initialize appStartupObjects array. */
+        /* Initialize an array for the singleAppStartup objects. */
         $appStartupObjects = array();
-
         /* Search registry for any data classified as an app component. */
         foreach (array_keys($this->crud->getRegistry()) as $storageId) {
-            /* Check if the data's classification indicates an app component. */
-            if ($this->crud->getRegistryData($storageId, 'classification') === 'DarlingCms\classes\component\app') {
-                /* Load the app component. */
-                if (($app = $this->crud->read($storageId)) !== false) {
-                    /* Create a singleAppStartup instance for the app component and add it to the $appStartupObjects array. */
-                    array_push($appStartupObjects, new \DarlingCms\classes\startup\singleAppStartup($app));
-                }
+            /* Load the app component. */
+            if (($app = $this->crud->read($storageId, 'DarlingCms\classes\component\app')) !== false) {
+                /* Create a singleAppStartup instance for the app component and add it to the $appStartupObjects array. */
+                array_push($appStartupObjects, new \DarlingCms\classes\startup\singleAppStartup($app));
             }
         }
-
         /* Determine if this is a fresh install of the Darling Cms based on whether
            or not any single app startup objects were instantiated for any app
            components registered with the crud. The assumptions is, since apps
@@ -176,7 +222,6 @@ class dcmsInitializer extends Ainitializer
         if ((empty($appStartupObjects)) === true) {
             return $this->handleFreshInstall();
         }
-
         /* Add app startup objects array to the initialized array. */
         return $this->setInitialized($appStartupObjects, 'appStartupObjects');
     }
@@ -195,8 +240,20 @@ class dcmsInitializer extends Ainitializer
      * @return bool True if all installed apps were successfully configured, false otherwise. A successful
      * configuration means that an app component was created for each app, and that app component was
      * successfully enabled and registered with the crud.
+     * @see \DarlingCms\classes\component\html\htmlHead
+     * @see \DarlingCms\classes\component\html\htmlHead::prependHtml()
+     * @see \DarlingCms\classes\component\html\html
+     * @see \DarlingCms\classes\userInterface\userInterface
+     * @see \DarlingCms\classes\userInterface\userInterface::addOpeningHtml()
+     * @see \DarlingCms\classes\component\html\htmlContainer
+     * @see dcmsInitializer::determineInstalledApps()
+     * @see \DarlingCms\classes\component\html\htmlContainer::appendHtml()
+     * @see \DarlingCms\classes\userInterface\userInterface::getUserInterface()
+     * @see \DarlingCms\classes\installer\appInstaller
+     * @see \DarlingCms\classes\installer\appInstaller::enableApp()
+     * @see \DarlingCms\classes\installer\appInstaller::install()
      */
-    private function handleFreshInstall()
+    private function handleFreshInstall(): bool
     {
         /* Instantiate the htmlHead object used to generate the pages html header.*/
         $htmlHead = new \DarlingCms\classes\component\html\htmlHead($this->getCrud());
@@ -232,12 +289,12 @@ class dcmsInitializer extends Ainitializer
                             $status[] = boolval(require $appInstaller);
                             break;
                         default:
-                            /* Create an app component for the app. */
-                            $app = new \DarlingCms\classes\component\app($appName);
-                            /* Enable the app. Track success or failure via the $status array. */
-                            $status[] = $app->enableApp();
-                            /* Register the new app component with the crud. Track success or failure via the $status array. */
-                            $status[] = $this->crud->create($appName, $app);
+                            /* Instantiate an app installer for the app. */
+                            $appInstaller = new appInstaller($appName, $this->crud);
+                            /* Enable the app. */
+                            $status[] = $appInstaller->enableApp();
+                            /* Install the app. */
+                            $status[] = $appInstaller->install();
                             break;
                     }
                 }
@@ -281,7 +338,7 @@ class dcmsInitializer extends Ainitializer
      * Determines what apps are installed.
      * @return array Array of installed app names.
      */
-    private function determineInstalledApps()
+    private function determineInstalledApps(): array
     {
         return array_unique(array_filter(scandir(__DIR__ . '/../../../apps'), array($this, 'stripDots')));
     }
