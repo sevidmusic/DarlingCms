c3a83f9b9b2281c9e95b735f7c03978c55376de1
sevidmusic <sevidmusic@gmail.com>
2018-05-01 12:53:27 -0500

    darlingCms_0.1_dev|index.php: Index.php is now using an instance of the CoreHtmlUserInterface class to generate it's output.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-
 /** Require Composer's auto-loader. **/
 require(__DIR__ . '/vendor/autoload.php');
+$CoreHtmlUserInterface = new \DarlingCms\classes\userInterface\CoreHtmlUserInterface(new \DarlingCms\classes\startup\MultiAppStartup());
+echo $CoreHtmlUserInterface->getUserInterface();
