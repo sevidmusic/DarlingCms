b5daac9203866f0555cdddcc71540d64a355f10a
sevidmusic <sevidmusic@gmail.com>
2018-05-26 00:02:41 -0500

    darlingCms_0.1_dev|core|classes: Defined new class Hidden which defines an implementation of the Input class that generates a hidden input element.

---
+++ core/classes/html/form/Hidden.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by Sevi Donnelly Foreman.
+ * Date: 5/25/18
+ * Time: 11:55 PM
+ */
+
+namespace DarlingCms\classes\html\form;
+
+/**
+ * Class Hidden. Defines an implementation of the Input class that generates a hidden input element.
+ * @package DarlingCms\classes\html\form
+ */
+class Hidden extends Input
+{
+    /**
+     * Hidden constructor. Sets the hidden input element's name, and value.
+     * @param string $name The hidden input element's name attribute's value.
+     * @param string $value The hidden input element's value attribute's value.
+     */
+    public function __construct(string $name, string $value)
+    {
+        parent::__construct('hidden', $name, $value, array());
+    }
+}
