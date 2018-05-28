007f9768732ae4fd477601bf472779a465859f49
sevidmusic <sevidmusic@gmail.com>
2018-05-27 22:36:02 -0500

    darlingCms_0.1_dev|core|interfaces: Defined new interface IFormProcessor that defines the basic contract of an object that processes a form.

---
+++ core/interfaces/processor/IFormProcessor.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by Sevi Dolnnelly Foreman.
+ * Date: 5/26/18
+ * Time: 10:24 PM
+ */
+
+namespace DarlingCms\interfaces\processor;
+
+/**
+ * Interface IFormProcessor. Defines the basic contract of an object that processes a form.
+ * @package DarlingCms\interfaces\processor
+ */
+interface IFormProcessor
+{
+    /**
+     * Determines if the form to be processed was submitted.
+     * @return bool True if the form to be processed was submitted, false otherwise.
+     */
+    public function formSubmitted(): bool;
+
+    /**
+     * Returns an array of submitted values, or an empty array if there are no submitted values.
+     * @return array An array of submitted values, or an empty array if there are no submitted values.
+     */
+    public function getSubmittedValues(): array;
+
+    /**
+     * Processes the form.
+     * @return bool True if form was processed successfully, false otherwise.
+     */
+    public function processForm(): bool;
+
+    /**
+     * Returns an array of the values that were successfully processed, or an empty array if
+     * no values were processed.
+     * @return array An array of the values that were successfully processed, or an empty array
+     *               if no values were processed.
+     */
+    public function getProcessedValues(): array;
+}
