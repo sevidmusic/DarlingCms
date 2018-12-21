d44caafde37255f80d9c78c1955c578a4fab42bf
sevidmusic <sevidmusic@gmail.com>
2018-12-20 21:50:24 -0500

    darlingCms_0.1_dev|core|interfaces: Created new interface IActionCrud which defines the contract of an object that is responsible for mapping IAction implementation instances so they can be created in, read from, updated in, and deleted from a data source such as a database.

---
+++ core/interfaces/crud/IActionCrud.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sevidmusic
+ * Date: 2018-12-20
+ * Time: 19:38
+ */
+
+namespace DarlingCms\interfaces\crud;
+
+
+use DarlingCms\interfaces\privilege\IAction;
+
+/**
+ * Interface IActionCrud. Defines the contract of an object that is responsible for
+ * mapping IAction implementation instances so they can be created in, read from,
+ * updated in, and deleted from a data source such as a database.
+ * @package DarlingCms\interfaces\crud
+ */
+interface IActionCrud
+{
+    public function create(IAction $action): bool;
+
+    public function read(string $actionName): IAction;
+
+    public function update(string $actionName, IAction $newAction): bool;
+
+    public function delete(string $actionName): bool;
+}
