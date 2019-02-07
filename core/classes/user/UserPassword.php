2ff68bc7eaafd579baff4bbb0ec7599d038946f5
sevidmusic <sevidmusic@gmail.com>
2019-02-07 01:39:37 -0500

    darlingCms_0.1_dev|core|classes: Refactored the UserPassword class. - Refactored the validatePassword() method to no longer check the passwordId as well as the password. This fixes the issue of password's no longer validating even when correct after a user has been updated as a result of the passwordId being based off the userId which is generated by the User class every time a user is instantiated. This change was important because the UserPassword class MUST NOT be required to be aware of the implementation details of a User.

--- core/classes/user/UserPassword.php
+++ core/classes/user/UserPassword.php
@@ -27,9 +27,27 @@ class UserPassword extends APDOCompatibleUserPassword implements IUserPassword
      */
     public function validatePassword(IUser $user, string $password): bool
     {
-        if (password_verify($user->getUserId(), $this->passwordId) === true && password_verify($password, $this->password) === true) {
+        if (password_verify($password, $this->password) === true) {
             return true;
         }
+        /**
+         * NOTE: Checking the password id as well as the password is ideal however, this behavior currently breaks
+         * if user has been modified. This is because the passwordId is just a hashed version of the userId, which
+         * in turn, is a randomly generated string based on a serialized version of the User as it was when it was
+         * originally created. @see APDOCompatibleUser::generateUserId()
+         *
+         * If a user is created, then updated later, the hashed password id will always fail to match the
+         * updated user as it was based on the state of the user before the updates occurred.
+         *
+         * Now, this is quite annoying because it is really nice to have a second test via the hashed passwordId,
+         * but the UserPassword class MUST NOT be aware of the implementation details of a User. So until a work
+         * around that does NOT involve refactoring the User and UserPassword classes to be more tightly coupled,
+         * the fix is to remove the passwordId check.
+         *
+         * Removing the passwordId check fixes the issue as the password is just a hash of the users actual password
+         * which does not changed whenever a stored User is updated.
+         * if (password_verify($user->getUserId(), $this->passwordId) === true && password_verify($password, $this->password) === true) {return true;}
+         */
         return false;
     }
 
