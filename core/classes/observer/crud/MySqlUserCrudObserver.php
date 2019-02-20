ac34143602dbcc9469b610531cb27825de22e765
sevidmusic <sevidmusic@gmail.com>
2019-02-20 11:14:55 -0500

    darlingCms_0.1_dev|core|classes: Refactored the MySqlUserCrudObserver class: - Fixed incorrect database name for database connection. - The update method now logs an error if it is passed a SplSubject that is not an AMySqlUserCrud implementation instance. - Added/revised doc comments.

--- core/classes/observer/crud/MySqlUserCrudObserver.php
+++ core/classes/observer/crud/MySqlUserCrudObserver.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sevidmusic
+ * Created by Sevi Donnelly Foreman.
  * Date: 2019-02-08
  * Time: 01:43
  */
@@ -9,36 +8,59 @@
 namespace DarlingCms\classes\observer\crud;
 
 
-use DarlingCms\classes\crud\MySqlUserPasswordCrud;
+use DarlingCms\classes\database\SQL\MySqlQuery;
 use DarlingCms\classes\staticClasses\core\CoreMySqlQuery;
 use DarlingCms\classes\staticClasses\core\CoreValues;
-use DarlingCms\classes\user\UserPassword;
 use SplSubject;
 use DarlingCms\abstractions\crud\AMySqlUserCrud;
 
+/**
+ * Class MySqlUserCrudObserver. This class is responsible for observing when a user is updated
+ * via an AMySqlUserCrud implementation instance. This class will perform the required updates
+ * to the user's corresponding password data whenever a user is updated.
+ * @package DarlingCms\classes\observer\crud
+ */
 class MySqlUserCrudObserver implements \SplObserver
 {
 
+    /**
+     * @var MySqlQuery Instance of a MySqlQuery implementation.
+     */
     private $mySqlQuery;
 
     /**
-     * MySqlUserCrudObserver constructor.
-     * @param MySqlUserPasswordCrud $passwordCrud
+     * MySqlUserCrudObserver constructor. Instantiates the MySqlQuery instance used to establish
+     * a connection to the database password data is stored in.
+     * @devNote: This class uses the CoreValues class to determine the name of the passwords database,
+     *           and to get the required MySqlQuery instance. CoreValues is used to insure multiple
+     *           instances of this class to not result in multiple database connections being opened
+     *           to the same database.
+     * @see MySqlQuery
+     * @see CoreValues
      */
     public function __construct()
     {
-        $this->mySqlQuery = CoreMySqlQuery::DbConnection(CoreValues::getUsersDBName());
+        /**
+         * Establish connection to passwords database.
+         * @devNote: The connection to the password database is established via a MySqlQuery instance
+         *           as opposed to a MySqlUserPasswordCrud instance because only the password id is
+         *           updated by this observer, the rest of the password data MUST not be modified by
+         *           instances of this class.
+         */
+        $this->mySqlQuery = CoreMySqlQuery::DbConnection(CoreValues::getPasswordsDBName());
     }
 
 
     /**
-     * Receive update from subject
-     * @link https://php.net/manual/en/splobserver.update.php
-     * @param SplSubject $subject <p>
-     * The <b>SplSubject</b> notifying the observer of an update.
-     * </p>
+     * Receive update from an AMySqlUserCrud implementation instance.
+     * @param SplSubject $subject The AMySqlUserCrud implementation instance that issued the notice to update.
+     *
+     *                            WARNING: To conform the the SplObserver interface the
+     *                            $subject parameter will accept any instance of an SplSubject,
+     *                            however, this method will only perform as intended if an
+     *                            of the AMySqlUserCrud implementation instance is passed to
+     *                            the $subject parameter.
      * @return void
-     * @since 5.1.0
      */
     public function update(SplSubject $subject)
     {
@@ -59,6 +81,8 @@ class MySqlUserCrudObserver implements \SplObserver
                     error_log('MySqlUserCrudObserver Error: Invalid modification type.');
                     break;
             }
+        } else {
+            error_log('MySqlUserCrudObserver Error: Failed to update user ' . $subject->user->getUserName() . '\'s corresponding password data. WARNING: This may lock the user out of their account.' . ' | User Id: ' . $subject->user->getUserId() . ' | Subject type was not valid.');
         }
     }
 
