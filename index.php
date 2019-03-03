3b1990da6b53c2b375adc89288c1b3488e984ff6
sevidmusic <sevidmusic@gmail.com>
2019-03-02 20:31:46 -0500

    darlingCms_0.1_dev|core|classes: Refactored index.php to determine which UI to load based on whether or not the site has been configured.

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 /** Require Composer's auto-loader. **/
 require(__DIR__ . '/vendor/autoload.php');
-/** Initialize the user interface. **/
-$CoreHtmlUserInterface = new \DarlingCms\classes\userInterface\CoreHtmlUserInterface(new \DarlingCms\classes\startup\MultiAppStartup(new \DarlingCms\classes\info\AppInfo(\DarlingCms\classes\info\AppInfo::STARTUP_DEFAULT)));
-/** Display the user interface. **/
-echo $CoreHtmlUserInterface->getUserInterface();
+// Determine which UI to load based on whether or not the site has been configured
+switch (\DarlingCms\classes\staticClasses\core\CoreValues::siteConfigured()) {
+    case false:
+        // @todo: Implement installer so user can be guided through configuration process via UI...
+        echo "
+            <h1>Welcome to the Darling Cms</h1>
+            <p>Sorry, it looks like your Darling Cms site has not been configured.</p>
+            <p>Please create a configuration file to use your new Darling Cms installation.</p>
+            ";
+        break;
+    default:
+        /** Initialize the user interface. **/
+        $CoreHtmlUserInterface = new \DarlingCms\classes\userInterface\CoreHtmlUserInterface(new \DarlingCms\classes\startup\MultiAppStartup(new \DarlingCms\classes\info\AppInfo(\DarlingCms\classes\info\AppInfo::STARTUP_DEFAULT)));
+        /** Display the user interface. **/
+        echo $CoreHtmlUserInterface->getUserInterface();
+        break;
+}
+
