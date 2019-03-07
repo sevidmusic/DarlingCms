704b8972e515f67a5d536bfd415ab44992e8469d
sevidmusic <sevidmusic@gmail.com>
2019-03-07 10:50:38 -0500

    darlingCms_0.1_dev|core|classes: Defined new interface ISiteConfiguration which defines an object that can be used to organize ISiteConfigurationSetting implementation instances into a site configuration.

---
+++ core/interfaces/config/ISiteConfiguration.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by Sevi Donnelly Foreman.
+ * Date: 2019-03-03
+ * Time: 07:01
+ */
+
+namespace DarlingCms\interfaces\config;
+
+/**
+ * Interface ISiteConfiguration. Defines an object that can be used to organize
+ * ISiteConfigurationSetting implementation instances into a site configuration.
+ * @package DarlingCms\interfaces\config
+ */
+interface ISiteConfiguration
+{
+    /**
+     * Returns an array of the ISiteConfigurationSetting implementation instances
+     * that belong to this site configuration.
+     * @return array|ISiteConfigurationSetting[] An array of the ISiteConfigurationSetting
+     *                                           implementation instances that belong to this
+     *                                           site configuration.
+     */
+    public function getConfigurationSettings(): array;
+
+    /**
+     * Returns the name used to identify this site configuration.
+     * @return string The name used to identify this site configuration.
+     */
+    public function getConfigurationName(): string;
+
+    /**
+     * Returns a description of this site configuration.
+     * @return string A description of this site configuration.
+     */
+    public function getConfigurationDescription(): string;
+
+}
