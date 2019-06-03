0e85e8fbc89e47dee3c80e40d49a97dcc660fbc5
sevidmusic <sevidmusic@gmail.com>
2019-06-03 11:12:43 -0400

    darlingCms_0.1_dev|core|interfaces: Refactored the IAppStartup interface: - Added/revised doc comments. - Added new getAppConfig() method to interface which returns an instance of the app's IAppConfig implementation.

--- core/interfaces/startup/IAppStartup.php
+++ core/interfaces/startup/IAppStartup.php
@@ -7,12 +7,16 @@
 
 namespace DarlingCms\interfaces\startup;
 
+use DarlingCms\interfaces\accessControl\IAppConfig;
 use DarlingCms\interfaces\pathMap\IPathMap;
 
 /**
- * Class IAppStartup. Defines the basic contract of an object that is responsible for handling the startup, shutdown,
- * and restart logic of a Darling Cms app.
+ * Class IAppStartup. Defines the basic contract of an object that is
+ * responsible for handling the startup, shutdown, and restart logic
+ * of a Darling Cms app.
+ *
  * @package DarlingCms\interfaces\startup
+ *
  * @see IAppStartup::getCssPaths()
  * @see IAppStartup::getJsPaths()
  * @see IAppStartup::getAppOutput()
@@ -25,19 +29,29 @@ interface IAppStartup extends IStartup, IPathMap
 {
     /**
      * Returns an array of paths to the css files assigned to the app.
+     *
      * @return array Array of paths to the css files assigned to the app.
      */
     public function getCssPaths(): array;
 
     /**
      * Returns an array of paths to the javascript files assigned to the app.
+     *
      * @return array Array of paths to the javascript files assigned to the app.
      */
     public function getJsPaths(): array;
 
     /**
      * Returns the app's output.
+     *
      * @return string The app's output.
      */
     public function getAppOutput(): string;
+
+    /**
+     * Returns an instance of the app's IAppConfig implementation.
+     *
+     * @return IAppConfig An instance of the app's IAppConfig implementation.
+     */
+    public function getAppConfig(): IAppConfig;
 }
