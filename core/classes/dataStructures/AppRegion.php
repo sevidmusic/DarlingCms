7d55f09dd8d8bc76d40d8e6b29ea4eada6882133
sevidmusic <sevidmusic@gmail.com>
2019-06-05 20:10:33 -0400

    darlingCms_0.1_dev|core|classes: Defined new class AppRegion whcih defines a bare implementation of the IAppRegion interface that extends the AAppRegion abstract class to implement most of it's logic.

---
+++ core/classes/dataStructures/AppRegion.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace DarlingCms\classes\dataStructures;
+
+
+use DarlingCms\abstractions\dataStructures\AAppRegion;
+use DarlingCms\interfaces\dataStructures\IAppRegion;
+
+/**
+ * Class AppRegion. Defines a bare implementation of the IAppRegion interface
+ * that extends the AAppRegion abstract class to implement most of it's logic.
+ * @package DarlingCms\classes\dataStructures
+ */
+class AppRegion extends AAppRegion implements IAppRegion
+{
+
+}
