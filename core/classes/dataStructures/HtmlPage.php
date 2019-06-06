6e6f64a6689ddd890c3f82cdc20dfa7436e0a683
sevidmusic <sevidmusic@gmail.com>
2019-06-06 10:30:01 -0400

    darlingCms_0.1_dev|core|classes: Defined new class HtmlDevHomepage which defines a bare implementation of the IHtmlPage interface that extends the AHtmlPage abstract class to implement most of it's logic.

---
+++ core/classes/dataStructures/HtmlPage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DarlingCms\classes\dataStructures;
+
+use DarlingCms\abstractions\dataStructures\AHtmlPage;
+use DarlingCms\interfaces\dataStructures\IHtmlPage;
+
+/**
+ * Class HtmlDevHomepage. Defines a bare implementation of the IHtmlPage interface
+ * that extends the AHtmlPage abstract class to implement most of it's logic.
+ *
+ * @package DarlingCms\classes\dataStructures
+ */
+class HtmlPage extends AHtmlPage implements IHtmlPage
+{
+
+}
