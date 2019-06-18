82a9cf4bf1f025eff7c014fd1a63dc01f2a383e6
sevidmusic <sevidmusic@gmail.com>
2019-06-18 15:41:11 -0400

    darlingCms_0.1_dev|core|interfaces: Refactored the ISqlObjectQuery interface: - Added/revised doc comments. - The ISqlObjectQuery interface now extends the ISqlQuery interface. - Removed the executeQuery() and getDsn() methods from the ISqlObjectQuery interface, they are already defined by the parent ISqlQuery interface.

--- core/interfaces/database/SQL/ISqlObjectQuery.php
+++ core/interfaces/database/SQL/ISqlObjectQuery.php
@@ -7,30 +7,26 @@
 
 namespace DarlingCms\interfaces\database\SQL;
 
-
 use DarlingCms\interfaces\database\IPDO;
-use PDOStatement;
 
 /**
- * Interface ISQLQuery. Defines an extension of the IPDO interface that defines
- * the contract of an class that can query a database that uses SQL as it's
- * query language.
+ * Interface ISQLQuery. Defines an extension of the IPDO interface, and the
+ * ISqlQuery interface, that defines the basic contract of class that can
+ * query a database that uses SQL as it's query language, and can also
+ * construct object instances from the data it queries from the database.
  *
- * Note: Only classes that are specifically designed to interface a SQL database should
- *       implement this interface.
+ * Note: Only classes that are specifically designed to query a SQL database
+ *       with the intention of constructing object instances from data in a
+ *       database should implement this interface.
  *
  * @package DarlingCms\interfaces\database\SQL
+ *
+ * @see ISqlQuery::executeQuery()
+ * @see ISqlQuery::getDsn()
+ * @see ISqlObjectQuery::getClass()
  */
-interface ISqlObjectQuery extends IPDO
+interface ISqlObjectQuery extends IPDO, ISqlQuery
 {
-    /**
-     * Execute an SQL query.
-     * @param string $sql The SQL statement to run.
-     * @param array $params (optional) Any parameters that should be included in the query.
-     * @return PDOStatement A PDOStatement object representing the query's prepared statement.
-     */
-    public function executeQuery(string $sql, array $params = array()): PDOStatement;
-
     /**
      * Gets an instance of a specified class using data from the specified SQL query to
      * construct the instance.
@@ -46,24 +42,7 @@ interface ISqlObjectQuery extends IPDO
      *
      * @return mixed An instance of the specified class constructed from the data in the specified table.
      *
-     * @todo This should be moved to an interface called ISqlObjectQuery which is implemented by classes that
-     *       specifically handle constructing objects from the results of specific sql query.
-     *
-     * @todo This should only return classes that implement a IPDOCompatible interface, which needs to be defined.
      */
     public function getClass(string $sql, string $className, array $params = array(), array $ctor_args = array());
-
-    /**
-     * Generate a DSN string based on specified parameters.
-     *
-     * @param string $host The host.
-     *
-     * @param string $dbName The database name.
-     *
-     * @param string $charset (optional) The charset. Defaults to an empty string.
-     *
-     * @return string The DSN string that generated based on the specified parameters.
-     */
-    public static function getDsn(string $host, string $dbName, string $charset = ''): string;
 
 }
