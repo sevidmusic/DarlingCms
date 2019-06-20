485298d41ab2c02366572f29c8d732c4110c20b0
sevidmusic <sevidmusic@gmail.com>
2019-06-20 12:59:17 -0400

    darlingCms_0.1_dev|core|interfaces: Refactored the IActionCrud interface: - Added/revised doc comments.

--- core/interfaces/crud/IActionCrud.php
+++ core/interfaces/crud/IActionCrud.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sevidmusic
+ * Created by Sevi Darling.
  * Date: 2018-12-20
  * Time: 19:38
  */
@@ -13,19 +12,59 @@ use DarlingCms\interfaces\privilege\IAction;
 
 /**
  * Interface IActionCrud. Defines the contract of an object that is responsible for
- * mapping IAction implementation instances so they can be created in, read from,
- * updated in, and deleted from a data source such as a database.
+ * creating, reading, updating, and deleting from a data source such as a database.
+ *
  * @package DarlingCms\interfaces\crud
  */
 interface IActionCrud
 {
+    /**
+     * Create a new action.
+     *
+     * @param IAction $action The IAction implementation instance that
+     *                        represents the action to create.
+     *
+     * @return bool True if action was created, false otherwise
+     */
     public function create(IAction $action): bool;
 
+    /**
+     * Read a specified action from the database.
+     *
+     * @param string $actionName The name of the action to read.
+     *
+     * @return IAction An IAction implementation instance that represents the action,
+     *                 or a default IAction implementation instance.
+     */
     public function read(string $actionName): IAction;
 
-    public function readAll():array;
+    /**
+     * Returns an array of IAction implementation instances for each
+     * action stored in the database.
+     *
+     * @return array An array of IAction implementation instances for each
+     *               action stored in the database.
+     */
+    public function readAll(): array;
 
+    /**
+     * Update a specified action.
+     *
+     * @param string $actionName The action to update.
+     *
+     * @param IAction $newAction An IAction implementation instance that represents the
+     *                           updated action.
+     *
+     * @return bool True if action was updated successfully, false otherwise.
+     */
     public function update(string $actionName, IAction $newAction): bool;
 
+    /**
+     * Deletes the specified action.
+     *
+     * @param string $actionName The name of the action to delete.
+     *
+     * @return bool True if action was deleted, false otherwise.
+     */
     public function delete(string $actionName): bool;
 }
