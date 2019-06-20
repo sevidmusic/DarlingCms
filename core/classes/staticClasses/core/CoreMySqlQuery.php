3ae2f0faad057ff6f1e1be0eacfff50ef31deca3
sevidmusic <sevidmusic@gmail.com>
2019-06-20 18:47:31 -0400

    darlingCms_0.1_dev|core|classes: Re-implemented and refactored the CoreMySqlQuery class: - Added/revised doc comments.

--- core/classes/staticClasses/core/CoreMySqlQuery.php
+++ core/classes/staticClasses/core/CoreMySqlQuery.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sevidmusic
+ * Created by Sevi Darling.
  * Date: 2019-02-16
  * Time: 16:38
  */
@@ -11,27 +10,60 @@ namespace DarlingCms\classes\staticClasses\core;
 use DarlingCms\classes\database\SQL\MySqlQuery;
 
 /**
- * Class CoreMySqlQueryCrud. This class serves as a static factory that can be used to obtain MySqlQuery
- * implementation instances that can be used to query specified databases. This class insures that only
- * one MySqlQuery implementation instance is established for each database connection.
+ * Class CoreMySqlQueryCrud. This class serves as a static factory that can be used
+ * to obtain MySqlQuery implementation instances that can be used to query specified
+ * databases. This class insures that only one MySqlQuery implementation instance is
+ * established for each database connection.
  *
- * Note: The connections established by this class utilize the db configuration defined by the CoreValues
- * static class to determine the db user, db password, and db host.
+ * Note: The connections established by this class utilize the db configurations defined
+ * by the CoreValues static class to determine each database connection's parameters.
+ *
+ * Note: Only databases whose parameters are defined in the site's configuration file will
+ *       be considered valid.
+ *       i,e. only database's whose connection parameters can be determined by the following
+ *       CoreValues methods are supported:
+ *       - CoreValues::getDBHostName() // Note: If CoreValues::getDBHostName($databaseName)
+ *                                              returns an empty string then the database
+ *                                              is not supported.
+ *       - CoreValues::getDBUserName() // Note: If CoreValues::getDBUserName($databaseName)
+ *                                              returns an empty string then the database
+ *                                              is not supported.
+ *       - CoreValues::getDBPassword() // Note: If CoreValues::getDBPassword($databaseName)
+ *                                              returns an empty string then the database
+ *                                              is not supported.
+ *
+ *
  * @package DarlingCms\classes\staticClasses\core
+ *
  * @see CoreValues
+ * @see CoreValues::getDBHostName()
+ * @see CoreValues::getDBUserName()
+ * @see CoreValues::getDBPassword()
+ *
+ * @see CoreMySqlQuery::DbConnection()
  */
 class CoreMySqlQuery
 {
     /**
-     * @var array|MySqlQuery[]
+     * @var array|MySqlQuery[] Array of MySqlQuery implementation instances used
+     *                         by this instance.
      */
     private static $connections = array();
 
     /**
-     * Returns a MySqlQuery implementation instance that can be used to query the specified database.
-     * @param string $databaseName The name of the database to establish a connection to.
-     * @param string $charset (optional) The database's charset. Defaults to MySqlQuery::DEFAULT_CHARSET.
-     * @return MySqlQuery A MySqlQuery instance for the specified database.
+     * Returns a MySqlQuery implementation instance that can be used to query the
+     * specified database.
+     *
+     * @param string $databaseName The name of the database to establish a
+     *                             connection to.
+     *                             Note: Only databases whose parameters are
+     *                                   defined in the site's configuration
+     *                                   file will be considered valid.
+     *
+     * @param string $charset (optional) The database's charset.
+     *                                   Defaults to MySqlQuery::DEFAULT_CHARSET.
+     *
+     * @return MySqlQuery A MySqlQuery implementation instance for the specified database.
      */
     public static function DbConnection(string $databaseName, string $charset = MySqlQuery::DEFAULT_CHARSET): MySqlQuery
     {
