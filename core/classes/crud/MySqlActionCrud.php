43d008a07a0f7ac72e3633deb5a7b4f801cc8b1a
sevidmusic <sevidmusic@gmail.com>
2019-07-08 09:31:30 -0700

    darlingCms_0.1_dev|core|classes: Refactored the MySqlActionCrud class: - Added/revised doc comments. - The MySqlActionCrud class now explicitly implements the SplSubject, ISqlQueryCrud, and ISqlObjectQueryCrud interfaces. - Refactored the create() method to correctly format the action's name before storing it via the StringUtility class's filterAlphaNumeric() method. This insures only alphanumeric characters are used in the action's name when creating a new action. - Refactored the readAll() method to sort the actions it reads alphabetically by adding an ORDER BY clause to it's query.

--- core/classes/crud/MySqlActionCrud.php
+++ core/classes/crud/MySqlActionCrud.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by Sevi Donnelly Foreman.
+ * Created by Sevi Darling.
  * Date: 2018-12-28
  * Time: 05:12
  */
@@ -10,47 +10,62 @@ namespace DarlingCms\classes\crud;
 
 use DarlingCms\abstractions\crud\AMySqlActionCrud;
 use DarlingCms\classes\privilege\Action;
+use DarlingCms\classes\staticClasses\utility\StringUtility;
 use DarlingCms\interfaces\crud\IActionCrud;
+use DarlingCms\interfaces\crud\ISqlObjectQueryCrud;
+use DarlingCms\interfaces\crud\ISqlQueryCrud;
 use DarlingCms\interfaces\privilege\IAction;
 use PDO;
+use SplSubject;
 
 /**
- * Class MySqlActionCrud. Defines an implementation of the IActionCrud interface
- * that extends the AMySqlActionCrud abstract class. This implementation can be used
- * to perform CRUD operations on action data in a MySql database.
+ * Class MySqlActionCrud. Defines an implementation of the IActionCrud, SplSubject,
+ * ISqlQueryCrud, ISqlObjectQueryCrud interfaces that extends the AMySqlActionCrud
+ * abstract class. This implementation can be used to perform CRUD operations on
+ * IAction implementation instance data in a MySql database.
+ *
  * @package DarlingCms\classes\crud
  */
-class MySqlActionCrud extends AMySqlActionCrud implements IActionCrud
+class MySqlActionCrud extends AMySqlActionCrud implements IActionCrud, SplSubject, ISqlQueryCrud, ISqlObjectQueryCrud
 {
     /**
      * Create a new action.
-     * @param IAction $action The IAction implementation instance that represents the action.
+     *
+     * @param IAction $action The IAction implementation instance that represents
+     *                        the action.
+     *
      * @return bool True if action was created, false otherwise.
      */
     public function create(IAction $action): bool
     {
+        // remove bad characters, preserve whitespace
+        $formattedActionName = StringUtility::filterAlphaNumeric($action->getActionName(), true);
         // make sure a action with same name does not already exist.
-        if ($this->actionExists($action->getActionName()) === true) {
+        if ($this->actionExists($formattedActionName) === true) {
             return false;
         }
         // create user
         $this->MySqlQuery->executeQuery('INSERT INTO ' . $this->tableName .
             ' (actionName, actionDescription, IActionType) VALUES (?,?,?)',
             [
-                $action->getActionName(),
+                $formattedActionName,
                 $action->getActionDescription(),
                 $this->formatClassName(get_class($action))
             ]
         );
-        return $this->actionExists($action->getActionName());
+        return $this->actionExists($formattedActionName);
     }
 
     /**
-     * Read the specified action's data from the database and return an appropriate IAction implementation
-     * instance.
-     * @param string $actionName The name of the action to return an IAction implementation instance for.
-     * @return IAction The IAction implementation instance. If the specified action does not exist this
-     *                 method will return a default IAction instance that defines an action named "No Access".
+     * Read the specified action's data from the database and return an appropriate
+     * IAction implementation instance.
+     *
+     * @param string $actionName The name of the action read.
+     *
+     * @return IAction The IAction implementation instance that represents
+     *                 the specified action, or, if the specified action does
+     *                 not exist this method will return a default IAction
+     *                 instance that defines an action named "No Access".
      */
     public function read(string $actionName): IAction
     {
@@ -67,12 +82,15 @@ class MySqlActionCrud extends AMySqlActionCrud implements IActionCrud
     }
 
     /**
-     * Read all stored actions into an array.
-     * @return array|IAction[] An array of all stored IAction implementations.
+     * Read all stored actions and return an array of IAction implementations
+     * instances for all stored actions.
+     *
+     * @return array|IAction[] An array of IAction implementations instances
+     *                         for all stored actions.
      */
     public function readAll(): array
     {
-        $actionNames = $this->MySqlQuery->executeQuery('SELECT actionName FROM ' . $this->tableName)->fetchAll(PDO::FETCH_ASSOC);
+        $actionNames = $this->MySqlQuery->executeQuery('SELECT actionName FROM ' . $this->tableName . ' ORDER BY actionName ASC')->fetchAll(PDO::FETCH_ASSOC);
         $actions = array();
         foreach ($actionNames as $actionName) {
             array_push($actions, $this->read($actionName['actionName']));
@@ -82,8 +100,12 @@ class MySqlActionCrud extends AMySqlActionCrud implements IActionCrud
 
     /**
      * Update the specified action.
+     *
      * @param string $actionName The name of the action to update.
-     * @param IAction $newAction The IAction implementation instance that represents the new action.
+     *
+     * @param IAction $newAction The IAction implementation instance that represents
+     *                           the updated action.
+     *
      * @return bool True if action was updated, false otherwise.
      */
     public function update(string $actionName, IAction $newAction): bool
@@ -102,31 +124,39 @@ class MySqlActionCrud extends AMySqlActionCrud implements IActionCrud
 
     /**
      * Delete the specified action.
+     *
      * @param string $actionName The name of the action to delete.
+     *
      * @return bool True if action was deleted, false otherwise.
      */
     public function delete(string $actionName): bool
     {
         if ($this->actionExists($actionName) === true) {
             /**
-             * Only set observer properties if mod type is not set to update, this will prevent this
-             * method from overwriting said properties if they were already set by the update method.
-             * This precaution is necessary because the update() method uses this method as part of
-             * it's logic.
+             * Only set observer properties if mod type is not set to update, this
+             * will prevent this method from overwriting said properties if they were
+             * already set by the update method.
+             *
+             * This precaution is necessary because the update() method uses this method
+             * as part of it's logic.
              */
             if ($this->modType !== self::MOD_TYPE_UPDATE) {
                 $this->modType = self::MOD_TYPE_DELETE;
                 $this->originalAction = $this->read($actionName);
-                $this->modifiedAction = $this->originalAction; // in this context modified and original are the same since the action is being deleted.
+                /**
+                 * In the following context modified and original are the same
+                 * since the action is being deleted.
+                 */
+                $this->modifiedAction = $this->originalAction;
             }
             // Delete the action
             $this->MySqlQuery->executeQuery('DELETE FROM ' . $this->tableName . ' WHERE actionName=? LIMIT 1', [$actionName]);
             // If action was deleted successfully notify the observer and return true.
             if ($this->actionExists($actionName) === false) {
                 /**
-                 * Only notify observer of action deletion if mod type is set to delete, if mod type is
-                 * update DO NOT notify observer here, the update() method will notify the observer if
-                 * appropriate.
+                 * Only notify observer of action deletion if mod type is set to delete,
+                 * if mod type is update DO NOT notify observer here, the update() method
+                 * will notify the observer if appropriate.
                  */
                 if ($this->modType === self::MOD_TYPE_DELETE) {
                     $this->notify();
